de53d84296800294d3857d5eda268478ce5e8cbe
xibuz-jpg <islamxalikulovz@mail.ru>
2026-03-09 16:13:59 +0500

    fix cart url

--- config/urls.py
+++ config/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
 
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('cart/', views.cart, name='cart'),
+    path('cart/', views.cart_detail, name='cart'),
 ]
